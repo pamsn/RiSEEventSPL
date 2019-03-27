5eed1bb055a729ce84c9c595f5c62a89f87d5194
rio <rio@rios-MacBook-Pro-2.local>
2019-03-27 20:50:07 -0300

    project

---
+++ RiSEEventSPLCC/src/rise/splcc/repository/SubmissionAuthorRepository.java
@@ -0,0 +1,23 @@
+//#if ${InsertAuthors} == "T"
+package rise.splcc.repository;
+
+import java.util.List;
+
+import rise.splcc.data.SubmissionAuthor;
+import rise.splcc.exception.RepositoryException;
+import rise.splcc.exception.SubmissionAuthorNotFoundException;
+
+public interface SubmissionAuthorRepository {
+	public void insert(SubmissionAuthor submissionAuthor) throws RepositoryException;
+
+	public void remove(SubmissionAuthor submissionAuthor) throws SubmissionAuthorNotFoundException, RepositoryException;
+
+	public SubmissionAuthor search(SubmissionAuthor submissionAuthor) throws SubmissionAuthorNotFoundException, RepositoryException;
+	
+	public List<SubmissionAuthor> getSubmissionAuthors() throws RepositoryException;
+
+	public void update(SubmissionAuthor submissionAuthor) throws SubmissionAuthorNotFoundException, RepositoryException;
+
+	public boolean isThere(SubmissionAuthor submissionAuthor) throws RepositoryException;
+}
+//#endif
